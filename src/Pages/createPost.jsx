02168b58c25a51a116e75eca5f0949e909a0944b
Jay sawwalakhe <jaysawwalakhe1234@gmail.com>
2025-09-12 20:44:19 +0530

    Update createPost.jsx

--- src/Pages/createPost.jsx
+++ src/Pages/createPost.jsx
@@ -39,7 +39,7 @@ export default function CreatePost() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/post", {
+      const response = await fetch("http://mindwolf.up.railway.app/post", {
         method: "POST",
         body: data,
         credentials: "include", // 👈 send cookies if logged in
